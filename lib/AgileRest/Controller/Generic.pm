0faec35362c745a316949cfdd2ef021703c1c8fa
web2solutions <eduardo@web2solutions.com.br>
2015-04-22 05:58:24 -0300

    removed some empty spaces

--- lib/AgileRest/Controller/Generic.pm
+++ lib/AgileRest/Controller/Generic.pm
@@ -5,28 +5,23 @@ use AgileRest::Model::Generic;
 
 sub list {
   my $self = shift;
-
   my $API = $self->API;
-
   my $access_granted_message = $API->check_authorization( $self );
   if ( $access_granted_message ne 'granted' )
   {
     return $self->unauthorized( $access_granted_message );
   }
-
   my $app = $self->app;
   my $logger = $self->logger;
   my $transaction = $self->tx;
   my $req = $transaction->req;
   $API->branch( $req->headers->header('X-branch') || 'test' );
-
   my $model = AgileRest::Model::Generic->new(
     API => $API,
     item => $self->stash('item'),
     collection => $self->stash('collection'),
     logger => $logger
   );
-
   my $collection_data = $model->list( {
     count => $self->param('count'),
     posStart => $self->param('posStart'),
@@ -38,11 +33,9 @@ sub list {
     relational_id => undef,
     specific_append_sql_logic_select => undef,
   } );
-
   if ( defined( $collection_data->{error} ) ) {
     return $self->fail( $collection_data->{error} );
   }
-
   $self->expose_default_headers;
   $self->render(
     json => $collection_data
@@ -54,49 +47,38 @@ sub list {
   #  },
   #  xml  => {text => '<hello>world</hello>'}
   #);
-
-
   # Do something after the transaction has been finished
   $self->on(finish => sub {
     my $c = shift;
     $API->trackAccessLog( $c );
-    #$c->app->log->debug('We are done');
-    #my $logger = $c->logger;
-    #$logger->debug( '======> End of transaction.');
   });
 }
 
 
 sub read {
   my $self = shift;
-
   my $API = $self->API;
-
   my $access_granted_message = $API->check_authorization( $self );
   if ( $access_granted_message ne 'granted' )
   {
     return $self->unauthorized( $access_granted_message );
   }
-
   my $app = $self->app;
   my $logger = $self->logger;
   my $transaction = $self->tx;
   my $req = $transaction->req;
   $API->branch( $req->headers->header('X-branch') || 'test' );
-
   my $model = AgileRest::Model::Generic->new(
     API => $API,
     item => $self->stash('item'),
     collection => $self->stash('collection'),
     logger => $logger
   );
-
   my $item_data = $model->read( {
     columns => $self->param('columns'),
     item_id => $self->stash( $model->primary_key )
 
   } );
-
   if ( defined( $item_data->{error} ) )
   {
     if ($item_data->{error} eq 'resource_not_found')
@@ -111,13 +93,11 @@ sub read {
       return $self->fail( $item_data->{error} );
     }
   }
-
   $self->expose_default_headers;
   $self->render(
     json => $item_data
     ,status => 200
   );
-
   # Do something after the transaction has been finished
   $self->on(finish => sub {
     my $c = shift;
@@ -138,37 +118,30 @@ sub create {
   {
     return $self->unauthorized( $access_granted_message );
   }
-
   my $app = $self->app;
   my $logger = $self->logger;
   my $transaction = $self->tx;
   my $req = $transaction->req;
   $API->branch( $req->headers->header('X-branch') || 'test' );
-
   my $model = AgileRest::Model::Generic->new(
     API => $API,
     item => $self->stash('item'),
     collection => $self->stash('collection'),
     logger => $logger
   );
-
   my $hash = $self->param('hash') || return $self->fail( 'hash is a mandatory parameter for this end point' );
-
   my $item_data = $model->create( {
     hash => $hash
   } );
-
   if ( defined( $item_data->{error} ) )
   {
     return $self->fail( $item_data->{error} );
   }
-
   $self->expose_default_headers;
   $self->render(
     json => $item_data
     ,status => 200
   );
-
   # Do something after the transaction has been finished
   $self->on(finish => sub {
     my $c = shift;
@@ -185,39 +158,32 @@ sub update {
   {
     return $self->unauthorized( $access_granted_message );
   }
-
   my $app = $self->app;
   my $logger = $self->logger;
   my $transaction = $self->tx;
   my $req = $transaction->req;
   $API->branch( $req->headers->header('X-branch') || 'test' );
-
   my $model = AgileRest::Model::Generic->new(
     API => $API,
     item => $self->stash('item'),
     collection => $self->stash('collection'),
     logger => $logger
   );
-
   my $hash = $self->param('hash') || return $self->fail( 'hash is a mandatory parameter for this end point' );
   my $item_id = $self->stash( $model->primary_key ) || return $self->fail( $model->primary_key. ' parameter is missing on stash' );
-
   my $item_data = $model->update( {
     item_id => $self->stash( $model->primary_key ),
     hash => $hash
   } );
-
   if ( defined( $item_data->{error} ) )
   {
     return $self->fail( $item_data->{error} );
   }
-
   $self->expose_default_headers;
   $self->render(
     json => $item_data
     ,status => 200
   );
-
   # Do something after the transaction has been finished
   $self->on(finish => sub {
     my $c = shift;
