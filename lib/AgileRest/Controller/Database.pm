b263c3df2e6f028fc7320e89366f5d6996c34708
web2solutions <eduardo@web2solutions.com.br>
2015-06-17 00:51:57 -0300

    several changes on tables mapper class

--- lib/AgileRest/Controller/Database.pm
+++ lib/AgileRest/Controller/Database.pm
@@ -1,7 +1,8 @@
 package AgileRest::Controller::Database;
 use Mojo::Base 'Mojolicious::Controller';
 
-use Mojo::JSON;
+#use Mojo::JSON_XS; # Must be earlier than Mojo::JSON
+use JSON qw(decode_json encode_json from_json to_json);
 use Tie::IxHash;
 
 sub map {
@@ -41,7 +42,7 @@ sub map {
     }
 
 
-
+    my $table_name = $_;
     my @columns = @{ $API->get_table_schema( $_ )->{columns} };
     my @fkeys = $API->get_table_foreing_keys( $_ );
 
@@ -57,6 +58,9 @@ sub map {
         else
         {
 
+            #my $foreign_column_text = $fkey_hash->{foreign_column_name};
+            #my $foreign_column_value =
+
             my $found_fkey = 0;
             my @column_values;
             push @column_values, $new_maped_table_id;
@@ -69,11 +73,31 @@ sub map {
             push @column_values, $API->sqlToDhxFormMask( $column_hash->{type} );
             foreach my $fkey_hash ( @fkeys )
             {
+
+
                 if( $fkey_hash->{name} eq $column_hash->{name} )
                 {
+                    $logger->debug( 'nome da table: ' .$_  );
+                    $logger->debug( 'nome da fk: ' .$fkey_hash->{name}  );
+                    $logger->debug( 'nome da coluna : ' .$column_hash->{name}  );
+
+
+                    #foreign_column_name, propriedade text
+                    #foreign_column_value, propriedade value
+
+                    my $prop_value = $fkey_hash->{foreign_column_name}; # column_id value
+                    $prop_value =~ s/ //gi;
+
+                    my $prop_text = $prop_value; # column text
+                    $prop_text =~ s/_id//gi;
+                    $prop_text =~ s/_id//gi;
+
+                    #
+
                     push @column_values, 1;
                     push @column_values, $fkey_hash->{foreign_table_name};
-                    push @column_values, $fkey_hash->{foreign_column_name};
+                    push @column_values, $prop_text;
+                    push @column_values, $prop_value;
                     push @column_values, 'coro';
                     $found_fkey = 1;
                 }
@@ -81,6 +105,7 @@ sub map {
 
             if ( $found_fkey == 0) {
                 push @column_values, 0;
+                push @column_values, '';
                 push @column_values, '';
                 push @column_values, '';
                 push @column_values, $API->sqlToDhxGridType( $column_hash->{type} );
@@ -136,6 +161,33 @@ sub map {
                }
             }
 
+            my $is_fk = 0;
+            my $strSQLcheckIfFk = "select R.TABLE_NAME, R.COLUMN_NAME
+              from INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE u
+              inner join INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS FK
+                  on U.CONSTRAINT_CATALOG = FK.UNIQUE_CONSTRAINT_CATALOG
+                  and U.CONSTRAINT_SCHEMA = FK.UNIQUE_CONSTRAINT_SCHEMA
+                  and U.CONSTRAINT_NAME = FK.UNIQUE_CONSTRAINT_NAME
+              inner join INFORMATION_SCHEMA.KEY_COLUMN_USAGE R
+                  ON R.CONSTRAINT_CATALOG = FK.CONSTRAINT_CATALOG
+                  AND R.CONSTRAINT_SCHEMA = FK.CONSTRAINT_SCHEMA
+                  AND R.CONSTRAINT_NAME = FK.CONSTRAINT_NAME
+              WHERE U.COLUMN_NAME = ?
+
+                AND U.TABLE_SCHEMA = 'public'
+                AND U.TABLE_NAME = ?
+              ";
+            my $sthc = $dbh->prepare( $strSQLcheckIfFk, );
+            $sthc->execute( $column_hash->{name}, $table_name ) or $self->fail( $sthc->errstr );
+            while (  my $re = $sthc->fetchrow_hashref())
+            {
+              $is_fk = 1;
+            }
+            push @column_values, $is_fk;
+
+
+            $logger->debug( $self->dumper( @column_values ) );
+            $logger->debug( '-------------------' );
             my $new_maped_column_id = $API->Insert( {
                 table => 'agile_rest_column'
                 ,columns => '
@@ -150,17 +202,21 @@ sub map {
                   has_fk,
                   foreign_table_name,
                   foreign_column_name,
+                  foreign_column_value,
                   dhtmlx_grid_type
                   ,ordinal_position
                   ,numeric_precision
                   ,numeric_scale
                   ,is_nullable
                   ,"default"
+                  ,is_fk
                 '
-                ,placeholders => '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?'
+                ,placeholders => '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?'
                 ,primary_key => 'agile_rest_column_id'
                 ,values => [@column_values]
             } );
+
+
             push @maped_tables, $_;
         }
     }
@@ -193,10 +249,12 @@ sub model {
 
   my $dbh = $API->dbh;
 
-  my $model = {};
+  my $schema = {};
+  my $settings = {};
+  my $records = {};
 
 
-  my $strSQL = "SELECT * FROM agile_rest_table ORDER BY table_name ASC";
+  my $strSQL = "SELECT * FROM agile_rest_table ORDER BY output_ordering ASC";
 	my $sth = $dbh->prepare( $strSQL, );
 	$sth->execute(  ) or $self->fail( $sth->errstr );
 	while ( my $record = $sth->fetchrow_hashref())
@@ -207,78 +265,38 @@ sub model {
     my $table_name = $record->{"table_name"};
     my @columns;
 
-    $model->{''.$table_name.''} = {
+    $schema->{''.$table_name.''} = {
       columns => {},
       primary_key => {
 
       },
-      fields => [],
-      str_columns => ''
+      foreign_keys => {
+
+      }
     };
 
-    #$model->{''.$table_name.''}->{columns}->{test} = 1;
+     $settings->{''.$table_name.''} = {
+      form => {
+        template => []
+      }
+     };
+
+    #$schema->{''.$table_name.''}->{columns}->{test} = 1;
 
     my $strSQLcolumns = "SELECT * FROM agile_rest_column WHERE  agile_rest_table_id = ? ORDER BY ordinal_position ASC";
     my $sth2 = $dbh->prepare( $strSQLcolumns, );
     $sth2->execute( $table_id ) or $self->fail( $sth->errstr );
     while (  my $r = $sth2->fetchrow_hashref())
     {
+      my $is_fk = 0;
+      my $child_table = '';
+      my $child_column = '';
 
-
+      $is_fk = $r->{is_fk};
       #tie %{ $r }, 'Tie::IxHash';
 
+
       my $column_name = $r->{name};
-      #my $maxlength = $r->{maxlength};
-      #$logger->debug( $column_name );
-
-
-      if ( $r->{type} ne 'primary_key' ) {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''} = $r;
-        delete($model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{agile_rest_column_id});
-        delete($model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{agile_rest_table_id});
-        #agile_rest_table_id
-
-        delete($model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{name});
-      }
-      else
-      {
-        $model->{''.$table_name.''}->{primary_key}->{keyPath} = $r->{name};
-        $model->{''.$table_name.''}->{primary_key}->{autoIncrement} = \0;
-      }
-
-
-      if ( $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{unique} == 0) {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{unique} = \0;
-      }
-      else
-      {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{unique} = \1;
-      }
-      if ( $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{index} == 0) {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{index} = \0;
-      }
-      else
-      {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{index} = \1;
-      }
-      if ( $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required} == 0) {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required} = \0;
-      }
-      else
-      {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required} = \1;
-      }
-      if ( $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{has_fk} == 0) {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{has_fk} = \0;
-      }
-      else
-      {
-        $model->{''.$table_name.''}->{columns}->{''.$column_name.''}->{has_fk} = \1;
-      }
-
-
-
-
 
       my $field = {};
 
@@ -286,15 +304,32 @@ sub model {
       $logger->debug( $r->{type} );
 
       if ( $r->{type} eq 'primary_key' ) {
-        $field->{required} = \1;
-        $field->{mask_to_use} = 'integer';
-        $field->{validate} = 'NotEmpty';
+        $field->{required} = \0;
+        $field->{mask_to_use} = '';
+        $field->{validate} = '';
       }
       else
       {
-        $field->{required} = \0;
+        if ( $is_fk ) {
+         $field->{required} = \1;
+         $field->{validate} = 'NotEmpty';
+        }
+        else
+        {
+          if ( $r->{is_nullable} ) {
+            $field->{required} = \0;
+            $field->{validate} = '';
+          }
+          else
+          {
+             $field->{required} = \1;
+             $field->{validate} = 'NotEmpty';
+          }
+        }
+
+
         $field->{mask_to_use} = $API->sqlToDhxFormMask( $r->{type} );
-        $field->{validate} = '';
+        $field->{value} = $r->{default};
       }
 
 
@@ -302,9 +337,16 @@ sub model {
       $field->{type} = $API->sqlToDhxFormType( $r->{type} );
       $field->{name} = $column_name;
       $field->{tooltip} = '';
-      $field->{value} = '';
+
       $field->{maxLength} = $r->{maxlength};
       $field->{label} = $r->{dhtmlx_grid_header};
+
+      if ( $r->{type} eq 'integer' ) {
+        $field->{maxLength} = $r->{numeric_precision};
+        delete( $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{maxlength} );
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{maxlength} = $r->{numeric_precision};
+      }
+
 
       if ( $field->{type}  eq 'calendar') {
         $field->{dateFormat} = '%Y-%m-%d';
@@ -314,7 +356,116 @@ sub model {
       }
 
 
+      if ( $r->{type} ne 'primary_key' ) {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''} = $r;
+        delete($schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{agile_rest_column_id});
+        delete($schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{agile_rest_table_id});
+        #agile_rest_table_id
 
+        delete($schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{name});
+      }
+      else
+      {
+        $schema->{''.$table_name.''}->{primary_key}->{keyPath} = $r->{name};
+        $schema->{''.$table_name.''}->{primary_key}->{autoIncrement} = \0;
+      }
+
+
+      if ( $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{unique} == 0) {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{unique} = \0;
+      }
+      else
+      {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{unique} = \1;
+      }
+
+
+      if ( $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{index} == 0) {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{index} = \0;
+      }
+      else
+      {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{index} = \1;
+      }
+
+
+      if ( $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required} == 0) {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required} = \0;
+      }
+      else
+      {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required} = \1;
+      }
+
+
+      $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_column_name} =~ s/ //gi if defined($schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_column_name});
+      $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_column_value} =~ s/ //gi if defined($schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_column_value});
+
+      if ( $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{has_fk} == 0) {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{has_fk} = \0;
+      }
+      else
+      {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{has_fk} = \1;
+        delete($schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required});
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required} = \1;
+
+
+        my $prop_value = $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_column_value}; # column_id value
+        $prop_value =~ s/ //gi;
+
+        my $prop_text = $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_column_name}; # column text
+        $prop_text =~ s/_id//gi;
+        $prop_text =~ s/_id//gi;
+
+
+
+
+        my $foreign_column_text = $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_column_name};
+
+        $foreign_column_text =~ s/ //gi;
+        $foreign_column_text =~ s/_id//gi;
+
+        my $foreign_column_value = $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_column_value};
+        $foreign_column_value =~ s/ //gi;
+
+
+        $schema->{''.$table_name.''}->{foreign_keys}->{''.$column_name.''} = {
+          table => $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_table_name}
+          ,column => $foreign_column_value
+          ,column_value => $foreign_column_value
+          ,column_text => $foreign_column_text
+        };
+
+        $field->{type} = 'combo';
+        $field->{options} = [];
+        $field->{validate} = 'NotEmpty';
+        $field->{required} = \1;
+        $field->{dhx_table} = $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{foreign_table_name};
+        $field->{dhx_prop_text} = $foreign_column_text;
+        $field->{dhx_prop_value} = $foreign_column_value;
+
+      }
+
+      if ( $is_fk == 1)
+      {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{is_fk} = \1;
+        delete($schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required});
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{required} = \1;
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{unique} = \1;
+
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{validate} = 'NotEmpty';
+
+        $field->{validate} = 'NotEmpty';
+        $field->{required} = \1;
+      }
+      else
+      {
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{is_fk} = \0;
+      }
+
+
+        $schema->{''.$table_name.''}->{columns}->{''.$column_name.''}->{name} = $column_name;
 
       #dateFormat:"%Y-%m-%d %H:%i"
 
@@ -324,26 +475,170 @@ sub model {
       push @columns, $column_name;
     }
 
-    delete($model->{''.$table_name.''}->{columns}->{''.$model->{''.$table_name.''}->{primary_key}->{keyPath}.''});
-    $model->{''.$table_name.''}->{fields} = [@fields];
-    $model->{''.$table_name.''}->{str_columns} = join( ',', @columns );
+    delete($schema->{''.$table_name.''}->{columns}->{''.$schema->{''.$table_name.''}->{primary_key}->{keyPath}.''});
+    $settings->{''.$table_name.''}->{form}->{template} = [@fields];
 	}
 
+  my @output_tables;
+  $strSQL = "SELECT * FROM agile_rest_table ORDER BY output_ordering ASC";
+	$sth = $dbh->prepare( $strSQL, );
+	$sth->execute(  ) or $self->fail( $sth->errstr );
+	while ( my $record = $sth->fetchrow_hashref())
+	{
 
-  #tie %{ $model }, 'Tie::IxHash';
+    my @fields;
+    my $table_id = $record->{"agile_rest_table_id"};
+    my $table_name = $record->{"table_name"};
+    my @columns;
 
-  $self->expose_default_headers;
+    my $output_obj = {};
+
+    $output_obj->{table_name} = $record->{table_name};
+    $output_obj->{output_ordering} = $record->{output_ordering};
+
+    push @output_tables, $output_obj;
+
+     $records->{''.$table_name.''} = [];
+
+     my @allrecords;
+
+
+    my $strSQLrecords = "SELECT * from $table_name";
+    my $sthc = $dbh->prepare( $strSQLrecords, );
+    $sthc->execute(  ) or $self->fail( $sth->errstr );
+    while (  my $re = $sthc->fetchrow_hashref())
+    {
+        push @allrecords, $re;
+    }
+
+    $records->{''.$table_name.''} = [@allrecords];
+  }
+
+
+  my @values;
+  my $version = 1;
+  $strSQL = "SELECT version FROM api_database_version ORDER BY version DESC LIMIT 1";
+	$sth = $dbh->prepare( $strSQL, );
+	$sth->execute(  ) or $self->fail( $sth->errstr );
+	while ( my $record = $sth->fetchrow_hashref())
+	{
+    $version = $record->{version} + 1;
+  }
+
+
+  push @values, $version;
+
+
   my $response = undef;
   $response = {
-          status => 'success',
-          response => 'model generated',
-          model => $model
+          #status => 'success',
+          #response => 'model generated',
+          db => undef
+          ,version => $version
+          ,schema => $schema
+          ,settings => $settings
+          #,records => $records
+          ,output_tables => [@output_tables]
+  };
+
+  push @values, to_json( $response );
+
+  my $new_version_id = $API->Insert( {
+      table => 'api_database_version'
+      ,columns => 'version, model'
+      ,placeholders => '?, ?'
+      ,primary_key => 'api_database_version_id'
+      ,values => [@values]
+  } );
+
+
+  $response->{records} = $records;
+
+  #tie %{ $response }, 'Tie::IxHash';
+  $self->expose_default_headers;
+  $self->render(
+    json => $response,
+    status => 200
+  );
+}
+
+
+
+
+sub get_model {
+  my $self = shift;
+  my $API = $self->API;
+  #my $access_granted_message = $API->check_authorization( $self );
+  #if ( $access_granted_message ne 'granted' )
+  #{
+  #  return $self->unauthorized( $access_granted_message );
+  #}
+  my $logger = $self->logger;
+  my @tables = $API->get_tables();
+
+  my $dbh = $API->dbh;
+
+  my $schema = {};
+  my $settings = {};
+  my $records = {};
+
+  my $model;
+  my $model_hash;
+  my $version;
+  my $strSQL = "SELECT * FROM api_database_version ORDER BY version DESC LIMIT 1";
+	my $sth = $dbh->prepare( $strSQL, );
+	$sth->execute(  ) or $self->fail( $sth->errstr );
+	while ( my $record = $sth->fetchrow_hashref())
+	{
+    $model = $record->{model};
+    $version = $record->{version};
+	}
+
+  $model_hash = from_json( $model );
+
+  my @output_tables;
+  $strSQL = "SELECT * FROM agile_rest_table ORDER BY output_ordering ASC";
+	$sth = $dbh->prepare( $strSQL, );
+	$sth->execute(  ) or $self->fail( $sth->errstr );
+	while ( my $record = $sth->fetchrow_hashref())
+	{
+
+    my @fields;
+    my $table_id = $record->{"agile_rest_table_id"};
+    my $table_name = $record->{"table_name"};
+    my @columns;
+
+
+    $records->{''.$table_name.''} = [];
+    my @allrecords;
+    my $strSQLrecords = "SELECT * from $table_name";
+    my $sthc = $dbh->prepare( $strSQLrecords, );
+    $sthc->execute(  ) or $self->fail( $sth->errstr );
+    while (  my $re = $sthc->fetchrow_hashref())
+    {
+        push @allrecords, $re;
+    }
+
+    $records->{''.$table_name.''} = [@allrecords];
+  }
+
+
+  my $response = undef;
+  $response = {
+          #status => 'success',
+          #response => 'model generated',
+          db => undef
+          ,version => $model_hash->{version}
+          ,schema => $model_hash->{schema}
+          ,settings => $model_hash->{settings}
+          ,records => $records
+          ,output_tables => [@{$model_hash->{output_tables}}]
+          ,status => 'success'
   };
 
 
 
-
-  #tie %{ $response }, 'Tie::IxHash';
+  $self->expose_default_headers;
 
   $self->render(
     json => $response,
